9e0cb10ab68924905f2fd69a0b62d440d8a68fe3
Rob <robert.tootill98@gmail.com>
2021-05-15 11:11:01 +0100

    added bounding box http request function

--- src/requests_HTTP.py
+++ src/requests_HTTP.py
@@ -8,6 +8,9 @@ config = yaml.safe_load(open("./config.yaml"))
 
 serverAddress = config['network']['server']['domain'] + ':' + str(config['network']['server']['port']) 
 
+###
+# gets access token for making authorized http requests
+###
 def login():
     global token
 
@@ -29,10 +32,19 @@ def login():
     data = json.loads(res.read().decode("utf-8"))
     token = data['access_token']
 
+###
 # adds a new vehicle record to web server db
+# @param {string} identifer of vehicle, decoded for vehicle record info
+###
 def addNewVehicle(identifier):
     # decode identifier string...
-    params = urllib.parse.urlencode({'identifier': identifier, 'entrance_id': 'id0', 'entrance_time': 0, 'exit_id': 'id1', 'exit_time': 1})
+    params = urllib.parse.urlencode({
+        'identifier': identifier, 
+        'entrance_id': 'id0',
+        'entrance_time': 0,
+        'exit_id': 'id1',
+        'exit_time': 1
+    })
 
     if(config['network']['server']['protocol'] == 'https'):
         conn = http.client.HTTPSConnection(serverAddress)
@@ -44,10 +56,38 @@ def addNewVehicle(identifier):
     res = conn.getresponse()
     print(res.status) 
 
+### 
+# update bounding box info for video stream
+# @param {string} id of the junction/camera
+# @param {double} height of bounding box
+# @param {double} width of bounding box
+# @param {double} x coord of top left of bounding box
+# @param {double} y coord of top left of bounding box
+###
+def updateBoundingBox(id, height, width, x, y):
+    params = urllib.parse.urlencode({
+        id: id,
+        height: height,
+        width: width,
+        x: x,
+        y: y
+    })
+
+    if(config['network']['server']['protocol'] == 'https'):
+        conn = http.client.HTTPSConnection(serverAddress)
+    else:
+        conn = http.client.HTTPConnection(serverAddress)
+
+    conn.request('POST', f'/api/vehicle/boundingbox?{params}', headers={'authorization': f'Bearer {token}'})
+
+    res = conn.getresponse()
+    print(res.status)
+
 # demo
 def main():
     login()
 
-    addNewVehicle("nothing")
+    #addNewVehicle("nothing")
+    updateBoundingBox('id0', 80, 60, 540.52, 120.28)
 
 main()
