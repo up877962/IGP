24ebc85466674fa85c8ed344855ce88b1e01976d
Rob <robert.tootill98@gmail.com>
2021-05-17 11:11:40 +0100

    requests module using python3 for compatibility with ffmpeg

--- src/requests_HTTP.py
+++ src/requests_HTTP.py
@@ -1,8 +1,10 @@
 # functions todo with making HTTP requests
 import yaml
 import json
-import httplib
-import urllib
+import http.client
+#import httplib
+import urllib.parse
+#import urllib
 
 config = yaml.safe_load(open("../config.yaml"))
 
@@ -12,7 +14,8 @@ serverAddress = config['network']['server']['domain'] + ':' + str(config['networ
 # gets access token for making authorized http requests
 ###
 def login():
-    global token
+
+    #global token
 
     # acquire sensitive information
     clientDomain = config['auth']['SPA']['domain']
@@ -21,7 +24,8 @@ def login():
     audience = config['auth']['API']['audience']
 
     # connect to domain
-    conn = httplib.HTTPSConnection(clientDomain)
+    conn = http.client.HTTPSConnection(clientDomain)
+    #conn = httplib.HTTPSConnection(clientDomain)
     # form request
     payload = '{"client_id":"' + clientID + '","client_secret":"' + clientSecret + '","audience":"' + audience  + '","grant_type":"client_credentials"}'
     headers = {'content-type': "application/json"}
@@ -31,32 +35,39 @@ def login():
     res = conn.getresponse()
     data = json.loads(res.read().decode("utf-8"))
     token = data['access_token']
+    return(token)
+
+def openConnection():
+   if(config['network']['server']['protocol'] == 'https'):
+       return http.client.HTTPSConnection(serverAddress)
+       #return httplib.HTTPSConnection(serverAddress)
+   else:
+       return http.client.HTTPSConnection(serverAddress)
+       #return httplib.HTTPConnection(serverAddress)
 
 ###
 # adds a new vehicle record to web server db
 # @param {string} identifer of vehicle, decoded for vehicle record info
 ###
-def addNewVehicle(identifier):
+def addNewVehicle(identifier, token):
     # decode identifier string...
-    params = urllib.urlencode({
-        'identifier': identifier, 
+    params = urllib.parse.urlencode({
+    #params = urllib.urlencode({
+        'identifier': identifier,
         'entrance_id': 'id0',
         'entrance_time': 0,
         'exit_id': 'id1',
         'exit_time': 1
     })
 
-    if(config['network']['server']['protocol'] == 'https'):
-        conn = httplib.HTTPSConnection(serverAddress)
-    else:
-	conn = httplib.HTTPConnection(serverAddress)
+    conn = openConnection()
 
     conn.request('POST', '/api/vehicle?' + params, headers={'authorization': 'Bearer ' + token})
 
     res = conn.getresponse()
-    print(res.status) 
+    print(res.status)
 
-### 
+###
 # update bounding box info for video stream
 # @param {string} id of the junction/camera
 # @param {double} height of bounding box
@@ -64,20 +75,17 @@ def addNewVehicle(identifier):
 # @param {double} x coord of top left of bounding box
 # @param {double} y coord of top left of bounding box
 ###
-def updateBoundingBox(id, height, width, x, y):
-    params = urllib.urlencode({
+def updateBoundingBox(id, height, width, x, y, token):
+    params = urllib.parse.urlencode({
+    #params = urllib.urlencode({
         'id': id,
         'height': height,
         'width': width,
         'x': x,
         'y': y
     })
-    print(params)
 
-    if(config['network']['server']['protocol'] == 'https'):
-        conn = httplib.HTTPSConnection(serverAddress)
-    else:
-        conn = httplib.HTTPConnection(serverAddress)
+    conn = openConnection()
 
     conn.request('POST', '/api/vehicle/boundingbox?' + params, headers={'authorization': 'Bearer ' + token})
 
@@ -85,11 +93,12 @@ def updateBoundingBox(id, height, width, x, y):
     print(res.status)
 
 # demo
-def main():
-    login()
+def demo():
+    token = login()
 
-    #print(token)
-    #addNewVehicle("nothing")
-    updateBoundingBox('id0', 80, 60, 540.52, 120.28)
+    print(token)
+    addNewVehicle("nothing", token)
+    updateBoundingBox('id0', 80, 60, 540.52, 120.28, token)
 
-main()
+demo()
+
